3b6bcf03031600c43dc353a9df5d9512be241271
Nikolaj Thorup Jensen <ntj@unknownborders.net>
2022-11-02 12:57:18 +0100

    changed to onLayoutReady, fixes issue with FC.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { App, Editor, EventRef, MarkdownPostProcessorContext, MarkdownView, Modal, Notice, Plugin, TFile } from 'obsidian';
-import { DEFAULT_SETTINGS, WeatherGeneratorSettings, wgData, wgWeatherData } from './settings';
+import { DEFAULT_SETTINGS, WeatherGeneratorSettings, wgData } from './settings';
 import { WeatherGeneratorSettingTab } from './settings/index';
 
 // declare module "obsidian" {
@@ -14,181 +14,158 @@ import { WeatherGeneratorSettingTab } from './settings/index';
 
 export default class WeatherGenerator extends Plugin {
 	private data: Record<string, boolean | string | WeatherGeneratorSettings>;
-	public hasFc: boolean;
 
 
 	async onload() {
 		console.log(`Weather Generator v${this.manifest.version} loaded.`);
-		await this.loadWeatherGeneratorData();
 
-		// if (app.plugins.getPlugin("fantasy-calendar")){
-		// 	app.plugins.getPlugin("fantasy-calendar").onSettingsLoad(() => {
-		// 		console.log("FC Enabled?")
-		// 	});
-		// }
-		// if(Object.keys(this.app.plugins.plugins).includes("fantasy-calendar")) {
-		// 	console.log("FC Enabled 2nd time")
-		// }
+		this.app.workspace.onLayoutReady(async () => {
+			await this.loadWeatherGeneratorData();
 
-		if (app.workspace.on("fantasy-calendars-settings-loaded", () => {
-			this.hasFc = true;
-		})) { }
-		else {
-			this.hasFc = false;
-		}
+			console.log("Is FC enabled and loaded: " + this.hasFantasyCalendar);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new WeatherGeneratorSettingTab(this.app, this));
+			this.addSettingTab(new WeatherGeneratorSettingTab(this.app, this));
 
-		//
-		// Start WEATHER GEN
-		//
-		this.registerMarkdownPostProcessor(
-			async (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
-				let nodeList = el.querySelectorAll("code");
+			this.registerMarkdownPostProcessor(
+				async (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+					console.log("Markdown Processor Started");
 
-				console.log("Markdown Processor Started");
-				console.log("Is FC enabled and loaded: " + this.hasFc);
+					let nodeList = el.querySelectorAll("code");
+					console.log(nodeList);
 
-				if (!nodeList.length) return;
+					if (!nodeList.length) return;
 
-				const path = ctx.sourcePath;
-				const info = ctx.getSectionInfo(el);
-				const lineStart = ctx.getSectionInfo(el)?.lineStart;
-				const file = this.app.vault.getAbstractFileByPath(ctx.sourcePath);
+					const path = ctx.sourcePath;
+					const info = ctx.getSectionInfo(el);
+					const lineStart = ctx.getSectionInfo(el)?.lineStart;
+					const file = this.app.vault.getAbstractFileByPath(ctx.sourcePath);
 
-				if (!file || !(file instanceof TFile)) return;
+					if (!file || !(file instanceof TFile)) return;
 
-				let fileContent: string[];
-				let weatherFound: boolean = false;
+					let fileContent: string[];
+					let weatherFound: boolean = false;
 
-				for (let i = 0; i < nodeList.length; i++) {
-					const node = nodeList[i];
+					for (let i = 0; i < nodeList.length; i++) {
+						const node = nodeList[i];
 
-					if (/weather-gen:/.test(node.innerText) && info) {
-						try {
-							const wgDataTemp = {
-								wgSeason: "",
-								fcSeason: "",
-								day: {
-									weather: "",
-									wind: "",
-									temp: "",
-									tempreg: ""
-								},
-								night: {
-									weather: "",
-									wind: "",
-									temp: "",
-									tempreg: ""
+						console.log("Iterating nodes, this is node: " + i + " out of: " + nodeList.length);
+						console.log(node);
+
+						if (/weather-gen:/.test(node.innerText) && info) {
+							try {
+								const wgDataTemp = {
+									wgSeason: "",
+									fcSeason: "",
+									day: {
+										weather: "",
+										wind: "",
+										temp: "",
+										tempreg: ""
+									},
+									night: {
+										weather: "",
+										wind: "",
+										temp: "",
+										tempreg: ""
+									}
+								};
+
+								const weatherArgs = node.innerText.split('--');
+
+								for (let index = 0; index < weatherArgs.length; index++) {
+									const arg = weatherArgs[index];
+									const item = arg.match(/(\w+)\s(.*)/);
+
+									if(!item) continue;
+
+									if(item[1] === "season") wgDataTemp.wgSeason = item[2].trimEnd();
+									else if (item[1] === "weather") wgDataTemp.day.weather = item[2].trimEnd();
+									else if (item[1] === "wind") wgDataTemp.day.wind = item[2].trimEnd();
+									else if (item[1] === "temp") wgDataTemp.day.temp = item[2].trimEnd();
+									else if (item[1] === "tempreg") wgDataTemp.day.tempreg = item[2].trimEnd();
+									else if (item[1] === "nweather") wgDataTemp.night.weather = item[2].trimEnd();
+									else if (item[1] === "nwind") wgDataTemp.night.wind = item[2].trimEnd();
+									else if (item[1] === "ntemp") wgDataTemp.night.temp = item[2].trimEnd();
+									else if (item[1] === "ntempreg") wgDataTemp.night.tempreg = item[2].trimEnd();
 								}
-							};
-
-							const weatherArgs = node.innerText.split('--');
-							
-							// console.log("Weather data INITIAL");
-							// console.log(wgDataTemp);
-
-
-							for (let index = 0; index < weatherArgs.length; index++) {
-								const arg = weatherArgs[index];
-								const item = arg.match(/(\w+)\s(.*)/);
-
-								if(!item) continue;
-
-								if(item[1] === "season") wgDataTemp.wgSeason = item[2].trimEnd();
-								else if (item[1] === "weather") wgDataTemp.day.weather = item[2].trimEnd();
-								else if (item[1] === "wind") wgDataTemp.day.wind = item[2].trimEnd();
-								else if (item[1] === "temp") wgDataTemp.day.temp = item[2].trimEnd();
-								else if (item[1] === "tempreg") wgDataTemp.day.tempreg = item[2].trimEnd();
-								else if (item[1] === "nweather") wgDataTemp.night.weather = item[2].trimEnd();
-								else if (item[1] === "nwind") wgDataTemp.night.wind = item[2].trimEnd();
-								else if (item[1] === "ntemp") wgDataTemp.night.temp = item[2].trimEnd();
-								else if (item[1] === "ntempreg") wgDataTemp.night.tempreg = item[2].trimEnd();
-							}
-							
-							console.log("Weather data BEFORE season");
-							console.log(wgDataTemp);
-							
-							if(!wgDataTemp.wgSeason) {
-								if (this.hasFc) {
-									wgDataTemp.fcSeason = this.getCurrentSeason().name;
-									const postIndex = this.getSettings().fantasyCalendarSeasons.findIndex(x => x.fcSeason == wgDataTemp.fcSeason);
-									wgDataTemp.wgSeason = this.getSettings().fantasyCalendarSeasons[postIndex].wgSeason;
+								
+								if(!wgDataTemp.wgSeason) {
+									if (this.hasFantasyCalendar) {
+										wgDataTemp.fcSeason = this.getCurrentSeason().name;
+										const postIndex = this.getSettings().fantasyCalendarSeasons.findIndex(x => x.fcSeason == wgDataTemp.fcSeason);
+										wgDataTemp.wgSeason = this.getSettings().fantasyCalendarSeasons[postIndex].wgSeason;
+									}
+									else {
+										new Notice("You must define a season using --season or make use of Fantasy Calendar");
+									}
 								}
-								else {
-									new Notice("You must define a season using --season or make use of Fantasy Calendar");
+
+								switch(wgDataTemp.wgSeason) {
+									case "wgWinter":
+										this.computeWinterWeatherData();
+										break;
+									case "wgSpring":
+										this.computeSpringWeatherData();
+										break;
+									case "wgSummer":
+										this.computeSummerWeatherData();
+										break;
+									case "wgFall":
+										this.computeFallWeatherData();
+										break;
+									default:
+										//new Notice("Season not recognized!")
+										console.log("Season not recognized!")
+										break;
 								}
-							}
-							
-							console.log("Weather data AFTER season");
-							console.log(wgDataTemp);
 
-							switch(wgDataTemp.wgSeason) {
-								case "wgWinter":
-									this.computeWinterWeatherData();
-									break;
-								case "wgSpring":
-									this.computeSpringWeatherData();
-									break;
-								case "wgSummer":
-									this.computeSummerWeatherData();
-									break;
-								case "wgFall":
-									this.computeFallWeatherData();
-									break;
-								default:
-									//new Notice("Season not recognized!")
-									console.log("Season not recognized!")
-									break;
-							}
+								if(!wgDataTemp.day.weather) {
+									// generate weather, based on season if available
+								}
 
-							if(!wgDataTemp.day.weather) {
-								// generate weather, based on season if available
-							}
+								if(!wgDataTemp.day.wind) {
+									// generate wind, based on weather and then season if available
+								}
 
-							if(!wgDataTemp.day.wind) {
-								// generate wind, based on weather and then season if available
-							}
+								if(!wgDataTemp.day.temp) {
+									// generate temp, based on weather then wind then season if available
+								}
+								
+								if(!wgDataTemp.day.tempreg) {
+									// generate tempreg, based on temp and season/location temp_low and temp_high if available
+								}
+								
+								if(!wgDataTemp.night.weather) {
+									// generate weather, based on season if available
+								}
+								
+								if(!wgDataTemp.night.wind) {
+									// generate wind, based on wind and then season if available
+								}
+								
+								if(!wgDataTemp.night.temp) {
+									// generate temp, based on weather then wind then season if available
+								}
+								
+								if(!wgDataTemp.night.tempreg) {
+									// generate tempreg, based on temp and season/location temp_low and temp_high if available
+								}
 
-							if(!wgDataTemp.day.temp) {
-								// generate temp, based on weather then wind then season if available
+							} catch (ex) {
+								console.error(ex);
 							}
-							
-							if(!wgDataTemp.day.tempreg) {
-								// generate tempreg, based on temp and season/location temp_low and temp_high if available
-							}
-							
-							if(!wgDataTemp.night.weather) {
-								// generate weather, based on season if available
-							}
-							
-							if(!wgDataTemp.night.wind) {
-								// generate wind, based on wind and then season if available
-							}
-							
-							if(!wgDataTemp.night.temp) {
-								// generate temp, based on weather then wind then season if available
-							}
-							
-							if(!wgDataTemp.night.tempreg) {
-								// generate tempreg, based on temp and season/location temp_low and temp_high if available
-							}
-
-						} catch (ex) {
-							console.error(ex);
 						}
 					}
+					console.log("Markdown Processor Ended");
 				}
-			}
-		)
+			)
+		});
+
+		
 	}
 
-	onLayoutReady() {
-		console.log("onLayoutReady");
-		if (app.plugins.getPlugin("fantasy-calendar")) {// checks for existance; all plugins are initialized in onLayoutReady
-			console.log(app.plugins.getPlugin("fantasy-calendar"));
-		}
+	get hasFantasyCalendar() {
+		return this.app.plugins.getPlugin("fantasy-calendar") != null;
 	}
 
 	capitalizeWords(arr) {
